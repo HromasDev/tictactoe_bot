321abdf445d72ebc655fb1a5f381540ba7658bb5
Hromas <90126334+HromasDev@users.noreply.github.com>
2024-11-05 21:15:23 +0600

    added express

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 require('dotenv').config();
+const express = require('express');
 const { Telegraf } = require('telegraf');
+
+const app = express();
+const port = process.env.PORT || 3000; // Порт, на котором будет работать сервер
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+// Обработчики команд бота
 bot.start((ctx) => ctx.reply('Добро пожаловать! Введите /play, чтобы начать игру.'));
 bot.command('play', (ctx) => {
   ctx.reply('Запускаем игру крестики-нолики, выберите вариант игры:', {
@@ -23,5 +28,16 @@ bot.command('play', (ctx) => {
   });
 });
 
+// Запуск бота
 bot.launch();
 console.log('Бот запущен');
+
+// Настройка маршрута для Express
+app.get('/', (req, res) => {
+  res.send('<h1>Бот запущен</h1>');
+});
+
+// Запуск сервера
+app.listen(port, () => {
+  console.log(`Сервер запущен на http://localhost:${port}`);
+});
